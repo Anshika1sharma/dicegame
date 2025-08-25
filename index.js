50ee806e9c379889398ab9c97fe433445116e42e
Anshika Sharma <140472829+Anshika1sharma@users.noreply.github.com>
2025-08-25 13:51:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ function gameStart(){
     var randomdice1 = Math.floor(Math.random() * 6) + 1;
     var randomdice2 = Math.floor(Math.random() * 6) + 1;
     switch(randomdice1){
-        case 1: document.querySelector(".dice1").setAttribute("src","./images/dice1.png");break;
-        case 2: document.querySelector(".dice1").setAttribute("src","./images/dice2.png");break;
-        case 3: document.querySelector(".dice1").setAttribute("src","./images/dice3.png");break;
-        case 4: document.querySelector(".dice1").setAttribute("src","./images/dice4.png");break;
-        case 5: document.querySelector(".dice1").setAttribute("src","./images/dice5.png");break;
-        case 6: document.querySelector(".dice1").setAttribute("src","./images/dice6.png");break;
+        case 1: document.querySelector(".dice1").setAttribute("src","./dice1.png");break;
+        case 2: document.querySelector(".dice1").setAttribute("src","./dice2.png");break;
+        case 3: document.querySelector(".dice1").setAttribute("src","./dice3.png");break;
+        case 4: document.querySelector(".dice1").setAttribute("src","./dice4.png");break;
+        case 5: document.querySelector(".dice1").setAttribute("src","./dice5.png");break;
+        case 6: document.querySelector(".dice1").setAttribute("src","./dice6.png");break;
     }
     switch(randomdice2){
-        case 1: document.querySelector(".dice2").setAttribute("src","./images/dice1.png");break;
-        case 2: document.querySelector(".dice2").setAttribute("src","./images/dice2.png");break;
-        case 3: document.querySelector(".dice2").setAttribute("src","./images/dice3.png");break;
-        case 4: document.querySelector(".dice2").setAttribute("src","./images/dice4.png");break;
-        case 5: document.querySelector(".dice2").setAttribute("src","./images/dice5.png");break;
-        case 6: document.querySelector(".dice2").setAttribute("src","./images/dice6.png");break;
+        case 1: document.querySelector(".dice2").setAttribute("src","./dice1.png");break;
+        case 2: document.querySelector(".dice2").setAttribute("src","./dice2.png");break;
+        case 3: document.querySelector(".dice2").setAttribute("src","./dice3.png");break;
+        case 4: document.querySelector(".dice2").setAttribute("src","./dice4.png");break;
+        case 5: document.querySelector(".dice2").setAttribute("src","./dice5.png");break;
+        case 6: document.querySelector(".dice2").setAttribute("src","./dice6.png");break;
     }
     if(randomdice1 > randomdice2){
         document.querySelector("h1").innerHTML="CONGRATULATIONS PLAYER 1 YOU WON ";
@@ -27,3 +27,4 @@ function gameStart(){
         document.querySelector("h1").innerHTML="DRAW";
     }
 }
+
